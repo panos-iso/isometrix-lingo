d6cbdf8bd8b348c964e496b573757b4f5d45f267
Panos Anastasiadis <anastapd@gmail.com>
2026-05-23 22:40:00 +0300

    Add Avalonia MVVM app with hello world message on .NET 10

---
+++ TranslationManagementTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TranslationManagementTool.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Hello World - Translation Management Tool";
+}
---
+++ TranslationManagementTool/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TranslationManagementTool.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
