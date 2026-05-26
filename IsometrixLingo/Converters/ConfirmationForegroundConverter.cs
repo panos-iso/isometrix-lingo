b47a5f9e80757614ca49954655b8d5e50e36d361
Panos Anastasiadis <anastapd@gmail.com>
2026-05-26 13:02:45 +0300

    Fix: Theme-aware confirmation colors - white in dark mode, black in light mode

--- IsometrixLingo/Converters/ConfirmationForegroundConverter.cs
+++ IsometrixLingo/Converters/ConfirmationForegroundConverter.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Globalization;
 using Avalonia;
-using Avalonia.Data;
 using Avalonia.Data.Converters;
 using Avalonia.Media;
+using Avalonia.Styling;
 
 namespace IsometrixLingo.Converters;
 
 /// <summary>
-/// Converter that returns a grey brush for modified keys, otherwise default foreground
+/// Converter that returns a grey brush for modified keys, otherwise theme-appropriate foreground
 /// </summary>
 public class ConfirmationForegroundConverter : IValueConverter
 {
     private static readonly SolidColorBrush GreyBrush = new(Color.Parse("#999999"));
+    private static readonly SolidColorBrush WhiteBrush = new(Colors.White);
+    private static readonly SolidColorBrush BlackBrush = new(Colors.Black);
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
@@ -21,7 +23,9 @@ public class ConfirmationForegroundConverter : IValueConverter
             return GreyBrush;
         }
 
-        return AvaloniaProperty.UnsetValue; // Use system default foreground
+        // Return white for dark mode, black for light mode
+        var theme = Application.Current?.ActualThemeVariant;
+        return theme == ThemeVariant.Dark ? WhiteBrush : BlackBrush;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
